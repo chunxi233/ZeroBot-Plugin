514482e0053ff5a7f28201c2009d9f3c32e1b0da
chunxi233 <108889710+chunxi233@users.noreply.github.com>
2022-07-08 10:21:01 +0800

    xzcsad

--- main.go
+++ main.go
@@ -200,8 +200,8 @@ func init() {
 	}
 
 	// 通过代码写死的方式添加主人账号
-	// sus = append(sus, 1844397051)
-	// sus = append(sus, 1693342099)
+	sus = append(sus, 1844397051)
+        sus = append(sus, 1693342099)
 
 	if *runcfg != "" {
 		f, err := os.Open(*runcfg)
